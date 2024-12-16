41281d54441579b470699bca6058c6e87b18b333
AHS <hesiar94@gmail.com>
2024-12-16 16:38:00 +0100

    Añadido comentario de solución alternativa a lista(2).py

--- nivel_1/lista(2).py
+++ nivel_1/lista(2).py
@@ -5,3 +5,4 @@ print("Yo estudio", end=" ")
 for asignatura in asignaturas:
     print(asignatura, end=", " if asignatura != asignaturas[-1] else "")
 
+# Otra forma: print("Yo estudio:", ", ".join(asignaturas)) // No hace falta hacer el foreach
